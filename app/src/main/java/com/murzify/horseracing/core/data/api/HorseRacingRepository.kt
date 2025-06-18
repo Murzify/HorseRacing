ea078a9d524254a79b359e7451f0751ced862225
Mikhail Murzin <murzify@gmail.com>
2025-06-18 20:11:56 +0500

    feat: define HorseRacingRepository result

--- app/src/main/java/com/murzify/horseracing/core/data/api/HorseRacingRepository.kt
+++ app/src/main/java/com/murzify/horseracing/core/data/api/HorseRacingRepository.kt
@@ -1,10 +1,11 @@
 package com.murzify.horseracing.core.data.api
 
 import com.murzify.horseracing.core.domain.model.RaceResult
+import kotlinx.coroutines.flow.Flow
 
 interface HorseRacingRepository {
 
-    fun getHistory(): List<RaceResult>
+    fun getHistory(): Flow<List<RaceResult>>
 
     fun saveResult(raceResult: RaceResult)
 }
